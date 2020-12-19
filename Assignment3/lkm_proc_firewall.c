00137f478b208ba8ffb0cd47fb618241c433b01c
Suho Lee <susemeee@gmail.com>
2020-12-19 23:19:33 +0900

    Fix forwarding

--- Assignment3/lkm_proc_firewall.c
+++ Assignment3/lkm_proc_firewall.c
@@ -191,10 +191,10 @@ static unsigned int _netfilter_hook_func(char rule_type, void* priv, struct sk_b
   }
 
   /** proxy */
-  if (is_in_netfilter_rules('P', sport) != 0) {
+  if (rule_type == 'I' && is_in_netfilter_rules('P', sport) != 0) {
     char* _daddr = FORWARD_NET_ADDR;
     ip_header->daddr = as_addr_to_net(daddr);
-    tcp_header->dest = tcp_header->source;
+    tcp_header->dest = htons(sport);
     ip_header->check = 0;
     ip_send_check(ip_header);
     printk(KERN_INFO "PROXY[%8s]: %d, %d, %d, %s, %s, %d%d%d%d", direction, ip_header->protocol, sport, dport, saddr, _daddr, syn, fin, ack, rst);
